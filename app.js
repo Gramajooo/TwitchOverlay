ec364aaa5b61d8602c16df93668f82f88c8b4de3
Jimena-chan <jiimenagramajo@gmail.com>
2020-11-07 20:41:31 -0600

    twitch chat integration when writing emotes

--- app.js
+++ app.js
@@ -1,112 +1,138 @@
-const target = document.querySelector('.target');
+const target = document.querySelector(".target");
+const leaderBoard = document.querySelector(".leader-board");
+
 const drops = [];
-const leaderBoard = document.querySelector('.leader-board');
+const currentUsers = {};
+let highScores = [];
 
 function createDropElement(url, isAvatar = false) {
-    const div = document.createElement('div');
-    div.className = 'drop';
-    div.innerHTML = `
+  const div = document.createElement("div");
+  div.className = "drop";
+  div.innerHTML = `
     <img class="chute" src="img/paracaidas.png" >
     <div class="user-image">
-        <img class="${isAvatar ? 'avatar' : ''}" 
+        <img class="${isAvatar ? "avatar" : ""}" 
         src="${url}" >
     </div>`;
-    return div;
+  return div;
 }
 
-
-function doDrop(url, isAvatar = false) {
-    const element = createDropElement(url, isAvatar);
-    const drop = {
-        id: Date.now() + Math.random(),
-        element,
-        location: {
-            x: window.innerWidth * Math.random(),
-            y: -200,
-        },
-        velocity: {
-            x: Math.random() * (Math.random() > 0.5 ? -1 : 1) * 5,
-            y: 2 + Math.random() * 2
-        }
-    };
-    drops.push(drop);
-    document.body.appendChild(element);
-    updateDropPosition(drop);
+function doDrop({ username, url, isAvatar = false }) {
+  currentUsers[username] = true;
+  const element = createDropElement(url, isAvatar);
+  const drop = {
+    id: username,
+    element,
+    location: {
+      x: window.innerWidth * Math.random(),
+      y: -200,
+    },
+    velocity: {
+      x: Math.random() * (Math.random() > 0.5 ? -1 : 1) * 5,
+      y: 2 + Math.random() * 2,
+    },
+  };
+  drops.push(drop);
+  document.body.appendChild(element);
+  updateDropPosition(drop);
 }
 
 function updateDropPosition(drop) {
-    if (drop.landed) return;
-    drop.element.style.top = drop.location.y + 'px';
-    drop.element.style.left = (drop.location.x - (drop.element.clientWidth / 2)) + 'px';
+  if (drop.landed) return;
+  drop.element.style.top = drop.location.y + "px";
+  drop.element.style.left =
+    drop.location.x - drop.element.clientWidth / 2 + "px";
 }
 
 function update() {
-    const targetHalftWidth = target.clientWidth / 2;
-    drops.forEach(drop => {
-        if (drop.landed) return;
-        drop.location.x += drop.velocity.x;
-        drop.location.y += drop.velocity.y;
-        const elementWidth = drop.element.clientWidth / 2;
-        if (drop.location.x + elementWidth >= window.innerWidth) {
-            drop.velocity.x = -Math.abs(drop.velocity.x);
-        } else if (drop.location.x - elementWidth < 0) {
-            drop.velocity.x = Math.abs(drop.velocity.x);
-        }
+  const targetHalftWidth = target.clientWidth / 2;
+  drops.forEach((drop) => {
+    if (drop.landed) return;
+    drop.location.x += drop.velocity.x;
+    drop.location.y += drop.velocity.y;
+    const elementWidth = drop.element.clientWidth / 2;
+    if (drop.location.x + elementWidth >= window.innerWidth) {
+      drop.velocity.x = -Math.abs(drop.velocity.x);
+    } else if (drop.location.x - elementWidth < 0) {
+      drop.velocity.x = Math.abs(drop.velocity.x);
+    }
 
-        if (drop.location.y + drop.element.clientHeight >= window.innerHeight) {
-            drop.velocity.x = 0;
-            drop.velocity.y = 0;
-            drop.location.y = window.innerHeight - drop.element.clientHeight;
-            drop.landed = true;
-            drop.element.classList.add('landed');
-            const { x } = drop.location;
-            //const score = ((1 - Math.abs(window.innerWidth / 2 - x)) / window.innerWidth / 2) * 100;
-            const score = Math.abs(window.innerWidth / 2 - x);
-            if (score <= targetHalftWidth) {
-                console.log('Target hit', drop);
-                const finalScore = (1 - (score / targetHalftWidth)) * 100;
-                console.log(finalScore);
-                leaderBoard.style.display = 'block';
-            }
-        }
-    });
+    if (drop.location.y + drop.element.clientHeight >= window.innerHeight) {
+      drop.velocity.x = 0;
+      drop.velocity.y = 0;
+      drop.location.y = window.innerHeight - drop.element.clientHeight;
+      drop.landed = true;
+      drop.element.classList.add("landed");
+      const { x } = drop.location;
+      //const score = ((1 - Math.abs(window.innerWidth / 2 - x)) / window.innerWidth / 2) * 100;
+      const score = Math.abs(window.innerWidth / 2 - x);
+      //   console.log(`Calculated width`, target.clientWidth);
+      if (score <= targetHalftWidth) {
+        // console.log("Target hit", drop);
+        const finalScore = (1 - score / targetHalftWidth) * 100;
+        console.log(finalScore);
+        leaderBoard.style.display = "block";
+        highScores.push({
+          username: drop.username,
+          score: finalScore.toFixed(2),
+        });
+        highScores.sort((a, b) => b.score - a.score);
+        highScores = highScores.slice(0, 5);
+        renderLeaderBoard();
+      }
+    }
+  });
+}
+
+function renderLeaderBoard() {
+  const scores = leaderBoard.querySelector(`.scores`);
+  scores.innerHTML = highScores.reduce((html, { score, username }) => {
+    return html + `<p> ${score} ${username} </p>`;
+  }, ``);
 }
 
 function draw() {
-    drops.forEach(updateDropPosition);
+  drops.forEach(updateDropPosition);
 }
 
-const emotes = [
-    'https://static-cdn.jtvnw.net/emoticons/v1/301379071/1.0',
-    'https://static-cdn.jtvnw.net/emoticons/v1/1436650/1.0',
-    'https://static-cdn.jtvnw.net/emoticons/v1/300507033/1.0',
-];
-
-emotes.forEach(doDrop);
-
-doDrop('https://static-cdn.jtvnw.net/jtv_user_pictures/3ab4df77-c5e0-4c9f-9bf0-19ed841a6fb2-profile_image-70x70.png', true);
-
 function gameLoop() {
-    update();
-    draw();
-    requestAnimationFrame(gameLoop);
+  update();
+  draw();
+  requestAnimationFrame(gameLoop);
 }
 
 gameLoop();
 
-
 const client = new tmi.Client({
-    connection: {
-        secure: true,
-        reconnect: true
-    },
-    channels: ['my-name']
+  connection: {
+    secure: true,
+    reconnect: true,
+  },
+  channels: ["Gramajooo"],
 });
 
-client.on('message', (channel, tags, message) => {
-    // console.log(`${tags['display-name']}: ${message}`);
+client.connect().catch(console.error);
 
-    if (message.startsWith('!drop')) {
-        console.log(tags.username, message, tags);
+client.on("message", (channel, { emotes, username }, message) => {
+  if (message.startsWith("!drop")) {
+    if (currentUsers[username]) return;
+    const args = message.split(" ");
+    args.shift();
+    const url = args.length ? args[0].trim() : "";
+
+    if (emotes) {
+      const emotesIds = Object.keys(emotes);
+      const emote = emotesIds[Math.floor(Math.random() * emotesIds.length)];
+      doDrop({
+        url: `https://static-cdn.jtvnw.net/emoticons/v1/${emote}/2.0`,
+        username,
+      });
+    } else {
+      console.log(username, url, emotes);
     }
+    
+} else {
+    // console.log(tags.username, message, tags)
+    // console.log(username, message, emote);
+  }
 });
